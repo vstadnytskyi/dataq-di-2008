522a42ea5e9d536f16cabb01ad920ca3bd90d0ac
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-04-23 00:08:41 +0000

    Update docs after building Travis build 10 of vstadnytskyi/dataq-di-2008
    The docs were built from the branch 'master' against the commit
    8d21299e1168d63c3d6975a69d1d201296972b8f.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/vstadnytskyi/dataq-di-2008/jobs/768051404.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.0.post20+g5c65d64',
+    VERSION: '0.0.0.post21+g8d21299',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
